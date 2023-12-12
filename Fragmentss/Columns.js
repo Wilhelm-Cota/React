cbe6903a1a2c3ba4b11fdbfec3b2a45dec79e411
Wilhelm-Cota <124986794+Wilhelm-Cota@users.noreply.github.com>
2023-12-12 09:52:38 +0200

    Fragments
    React uses fragments to remove unwanted DOM elements such as divs

---
+++ Fragmentss/Columns.js
@@ -0,0 +1,16 @@
+import React from "react";
+function Columnss(){
+    return (
+        <React.Fragment>
+        <td>Name</td>
+        <td>Surname</td>
+        <td>Name</td>
+        <td>Surname</td>
+        <td>Name</td>
+        <td>Surname</td>
+        <td>Name</td>
+        <td>Surname</td>
+    </React.Fragment>
+    )
+}
+export default Columnss;
---
+++ Fragmentss/Table.js
@@ -0,0 +1,14 @@
+import Columnss from "./Columns";
+
+function Table(){
+    return <div>
+        <table> 
+            <tbody>
+                <tr>
+                    <Columnss />
+                </tr>
+            </tbody>
+        </table>
+    </div>
+}
+export default Table;
